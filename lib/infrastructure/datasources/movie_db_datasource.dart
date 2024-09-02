9d0fe9f74a81840bda18dba9a86ef1b4676004a1
Alexander Mendivil <alexanderima@outlook.es>
2024-09-02 16:44:03 -0700

    api call

--- lib/infrastructure/datasources/movie_db_datasource.dart
+++ lib/infrastructure/datasources/movie_db_datasource.dart
@@ -9,10 +9,14 @@ class MovieDBDataSource extends MovieDataSource {
     'api_key': Environment.movieDBKey,
     'language': 'en-US',
   }));
-  
+
   @override
   Future<List<Movie>> getNowPlayingMovies({int page = 1}) async {
     try {
+      final response = await dio.get('/movie/now_playing', queryParameters: {
+        'page': page,
+      });
+      
       return [];
     } catch (e) {
       throw Exception(e);
