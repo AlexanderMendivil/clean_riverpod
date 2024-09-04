219da1154a3d6dc76a0b129abc56ee5063cd24b1
Alexander Mendivil <alexanderima@outlook.es>
2024-09-03 18:21:03 -0700

    riverpod on app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:clean_riverpod/router/app_router.dart';
 import 'package:clean_riverpod/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
