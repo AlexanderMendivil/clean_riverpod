3b504a464659216ee58d959bdc66284292648106
Alexander Mendivil <alexanderima@outlook.es>
2024-10-13 20:37:38 -0700

    screen wip

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -35,9 +35,76 @@ class _MovieScreenState extends ConsumerState<MovieScreen> {
               physics: const ClampingScrollPhysics(),
               slivers: [
                 _CustomSliverAppBar(movie: state.movie!),
+                SliverList(
+                    delegate: SliverChildBuilderDelegate(childCount: 1,
+                        (BuildContext _, int index) {
+                  return _MovieDetails(
+                    movie: state.movie!,
+                  );
+                })),
               ],
             ),
           );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    final textStyles = Theme.of(context).textTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(movie.posterPath, width: size.width * 0.3),
+              ),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: size.width * 0.6,
+                child: Column(
+                  children: [
+                    Text(
+                      movie.title,
+                      style: textStyles.titleLarge,
+                    ),
+                    Text(
+                      movie.overview!,
+                      style: textStyles.titleLarge,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((genre) => Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Chip(
+                      label: Text(genre),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+        const SizedBox(height: 100),
+      ],
+    );
   }
 }
 
@@ -62,32 +129,30 @@ class _CustomSliverAppBar extends StatelessWidget {
           children: [
             SizedBox.expand(
               child: Image.network(
-                movie.posterPath!,
+                movie.posterPath,
                 fit: BoxFit.cover,
               ),
             ),
             const SizedBox.expand(
               child: DecoratedBox(
-                  decoration:
-                      BoxDecoration(gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        stops: [0.7, 1.0],
-                        colors: [
-                        Colors.transparent,
-                        Colors.black87
-                      ]))),
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [0.7, 1.0],
+                          colors: [Colors.transparent, Colors.black87]))),
             ),
             const SizedBox.expand(
               child: DecoratedBox(
-                  decoration:
-                      BoxDecoration(gradient: LinearGradient(
-                        begin: Alignment.topLeft,                        
-                        stops: [0.0, 0.3],
-                        colors: [
-                        Colors.black87,
-                        Colors.transparent,
-                      ]))),
+                  decoration: BoxDecoration(
+                      gradient:
+                          LinearGradient(begin: Alignment.topLeft, stops: [
+                0.0,
+                0.3
+              ], colors: [
+                Colors.black87,
+                Colors.transparent,
+              ]))),
             )
           ],
         ),
