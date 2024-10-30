b2ad029f78fc810193a97e78d3da0ad2f5c5a337
Alexander Mendivil <alexanderima@outlook.es>
2024-10-29 21:06:00 -0700

    fade in

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,32 +1,43 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:clean_riverpod/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   String get searchFieldLabel => 'Search Movie';
   
   @override
   List<Widget>? buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
+    return [
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: ()=> query = '',
+        ),
+      )
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    throw UnimplementedError();
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
+    return const Text("some");
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return const Text("some");
   }
 
 }
