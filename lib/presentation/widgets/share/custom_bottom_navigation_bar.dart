d9ade638d52649dcb4674cda7d5775c0b73cf7da
Alexander Mendivil <alexanderima@outlook.es>
2024-11-09 12:17:19 -0700

    Added navigation

--- lib/presentation/widgets/share/custom_bottom_navigation_bar.dart
+++ lib/presentation/widgets/share/custom_bottom_navigation_bar.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar({super.key});
 
+  static int getCurrentIndex(BuildContext context){
+    final String location = GoRouterState.of(context).matchedLocation;
+
+print(location);
+    switch (location) {
+      case '/':
+        return 0;
+      case '/':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+      default:
+        context.go('/');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      elevation: 16.5,
-      items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
-      BottomNavigationBarItem(icon: Icon(Icons.label_outline), label: 'Categorias'),
-      BottomNavigationBarItem(icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
-    ]);
+    return BottomNavigationBar(      
+      type: BottomNavigationBarType.shifting,
+      currentIndex: getCurrentIndex(context),
+        elevation: 16,
+        onTap: (int index) => _onItemTapped(context, index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outline), label: 'Categorias'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
+        ]);
   }
 }
